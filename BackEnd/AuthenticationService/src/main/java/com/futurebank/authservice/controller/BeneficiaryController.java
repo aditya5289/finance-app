4804e257a03b02308196330c4d21804cd771fb6e
aditya5289 <90837943+aditya5289@users.noreply.github.com>
2024-02-15 19:13:31 +0530

    Update BeneficiaryController.java

--- BackEnd/AuthenticationService/src/main/java/com/futurebank/authservice/controller/BeneficiaryController.java
+++ BackEnd/AuthenticationService/src/main/java/com/futurebank/authservice/controller/BeneficiaryController.java
@@ -4,7 +4,7 @@ import com.futurebank.authservice.model.Beneficiary;
 import com.futurebank.authservice.service.BeneficiaryService;
 import com.futurebank.authservice.service.UserService; // Make sure you have this service
 
-import org.apache.catalina.User;
+import com.futurebank.authservice.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
